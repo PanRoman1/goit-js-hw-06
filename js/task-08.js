dfc68cf5905bb997a213cea2e2bc29820a7c6d71
PanRoman1 <panchuk1985@ukr.net>
2022-09-11 18:53:37 +0300

    Доопрацьоване ДЗ6

--- js/task-08.js
+++ js/task-08.js
@@ -4,35 +4,13 @@ form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(event) {
   event.preventDefault();
-
-  const emailRef = event.currentTarget.elements.email;
-  const passwordRef = event.currentTarget.elements.password;
-
-  let isEmptyInput = false;
-
-  if (emailRef.value === '') {
-    emailRef.classList.add('invalid-input');
-    emailRef.addEventListener('focus', onInputFocus);
-    isEmptyInput = true;
+  const {
+    elements: { email, password }
+    } = event.currentTarget;
+  if (email.value === "" || password.value === "") {
+    return alert('All fields must be filled in');
   }
 
-  if (passwordRef.value === '') {
-    passwordRef.classList.add('invalid-input');
-    passwordRef.addEventListener('focus', onInputFocus);
-    isEmptyInput = true;
-  }
-
-  function onInputFocus(event) {
-    event.currentTarget.classList.remove('invalid-input');
-  }
-
-  if (isEmptyInput) alert('All fields must be filled in');
-  else {
-    const formData = {
-      email: emailRef.value,
-      password: passwordRef.value,
-    };
-    console.log(formData);
-    event.currentTarget.reset();
-  }
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
 }
